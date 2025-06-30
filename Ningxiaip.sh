054ffbcf9848d390f0ed9dc1ff7660e8f760d928
ykyfy <3875056342@qq.com>
2025-06-30 13:28:56 +0800

    Update and rename CaijiHainanip.sh to Ningxiaip.sh

--- Ningxiaip.sh
+++ Ningxiaip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 海南（Hainan_dianxin）"
+  echo "1. 宁夏（Ningxia_dianxin）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -17,8 +17,8 @@ fi
 # 设置城市和相应的stream
 case $city_choice in
     1)
-        city="Hainan_dianxin"
-        stream="rtp/239.253.64.121:5140"
+        city="Ningxia_dianxin"
+        stream="rtp/239.121.4.94:8538"
         ;;
     0)
         # 逐个处理{ }内每个选项
